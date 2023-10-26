86819c01def0c91de41863e2ca4a0bd7019f3f70
Ratimon <ratixoxo@gmail.com>
2023-10-26 08:19:47 +0700

    Test (./tests): Adding alternate test suites for r1cs

--- tests/test_r1cs.py
+++ tests/test_r1cs.py
@@ -95,6 +95,57 @@ def test_polynomial():
     result2 = C.dot(w) - np.multiply(A.dot(w), B.dot(w)) == 0
     assert result2.all(), "system contains an inequality"
 
+def test_polynomial_alt():
+
+    # Transforming:
+    # out = 5*x^3 - 4*y^2*x^2 + 13*x*y^2 + x^2 - 10*y
+    # out = 5*v1*x - 4*v2*v1 + 13*x*v2 + v1 - 10*y
+
+    # v1 = x*x
+    # v2 = y*y
+    # v3 = 4*v1*v2
+    # v4 = -13*x*v2
+    
+    # v3 + v4 - v1 + 10*y + out = 5*v1*x
+    # out + 10*y - v1 + v3 + v4  = 5*v1*x
+
+    # Our witness vector is: [1 out x y v1 v2 v3 v4]
+
+    A = np.array([[0,0,1,0,0,0,0,0],
+                  [0,0,0,1,0,0,0,0],
+                  [0,0,0,0,4,0,0,0],
+                  [0,0,-13,0,0,0,0,0],
+                  [0,0,0,0,5,0,0,0]])
+    
+    B = np.array([[0,0,1,0,0,0,0,0],
+                  [0,0,0,1,0,0,0,0],
+                  [0,0,0,0,0,1,0,0],
+                  [0,0,0,0,0,1,0,0],
+                  [0,0,1,0,0,0,0,0]])
+    
+    C = np.array([[0,0,0,0,1,0,0,0],
+                  [0,0,0,0,0,1,0,0],
+                  [0,0,0,0,0,0,1,0],
+                  [0,0,0,0,0,0,0,1],
+                  [0,1,0,10,-1,0,1,1]])
+    
+    # pick random values for x and y
+    x = random.randint(1,1000)
+    y = random.randint(1,1000)
+
+    v1 = x*x
+    v2 = y*y
+    v3 = 4*v1*v2
+    v4 = -13*x*v2
+    out = 5*x*x*x - 4*y*y*x*x + 13*x*y*y + x*x - 10*y
+
+    w = np.array([1, out, x, y, v1, v2, v3, v4])
+
+    result = C.dot(w) == np.multiply(A.dot(w),B.dot(w))
+    assert result.all(), "result contains an inequality"
+
+    result2 = C.dot(w) - np.multiply(A.dot(w), B.dot(w)) == 0
+    assert result2.all(), "system contains an inequality"
 
 # fn main(x: field, y: field) -> field {
 #   assert!(y == 0 || y == 1 || y == 2);
